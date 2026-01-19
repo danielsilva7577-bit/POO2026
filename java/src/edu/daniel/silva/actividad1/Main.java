58ea701f804bd8144fb39a0484d6c5db3b63c3b2
danielsilva7577 bits <danielsilva7577@gmail.com>
2026-01-18 18:06:30 -0600

    Implementación de arquitectura en capas y entrada de usuario intuitiva
    Organización de archivos bajo el paquete edu.daniel.silva.actividad1.
    
    Separación de responsabilidades: Carro (data), ShopManager (logic) y CLI (ui).
    
    Mejora en la experiencia de usuario aceptando entradas numéricas y parciales.

--- java/src/edu/daniel/silva/actividad1/Main.java
+++ java/src/edu/daniel/silva/actividad1/Main.java
@@ -3,8 +3,7 @@ package edu.daniel.silva.actividad1;
 import edu.daniel.silva.actividad1.ui.CLI;
 
 public class Main {
-    public static void main(String[] args){
-        CLI.start();
+    public static void main(String[] args) {
+        CLI.launchApp();
     }
-
 }
